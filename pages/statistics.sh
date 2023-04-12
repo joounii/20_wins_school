b1426dab49fe884c6c7585fe9ae5ad5f0f725cc4
joounii <jonifu06@gmail.com>
2023-04-12 19:46:28 +0200

    updated statistics.sh file

--- pages/statistics.sh
+++ pages/statistics.sh
@@ -11,11 +11,20 @@ statistic_page () {
         lose_random=$(jq --arg get_name "$get_name" '.users[] | select(.name == $get_name) | .pve_random[].lose' ./data/user_statistics.json)
         win_impossible=$(jq --arg get_name "$get_name" '.users[] | select(.name == $get_name) | .pve_impossible[].win' ./data/user_statistics.json)
         lose_impossible=$(jq --arg get_name "$get_name" '.users[] | select(.name == $get_name) | .pve_impossible[].lose' ./data/user_statistics.json)
+        win_easy=$(jq --arg get_name "$get_name" '.users[] | select(.name == $get_name) | .pve_easy[].win' ./data/user_statistics.json)
+        lose_easy=$(jq --arg get_name "$get_name" '.users[] | select(.name == $get_name) | .pve_easy[].lose' ./data/user_statistics.json)
+        win_hard=$(jq --arg get_name "$get_name" '.users[] | select(.name == $get_name) | .pve_hard[].win' ./data/user_statistics.json)
+        lose_hard=$(jq --arg get_name "$get_name" '.users[] | select(.name == $get_name) | .pve_hard[].lose' ./data/user_statistics.json)
+        win_pvp=$(jq --arg get_name "$get_name" '.users[] | select(.name == $get_name) | .pve_pvp[].win' ./data/user_statistics.json)
+        lose_pvp=$(jq --arg get_name "$get_name" '.users[] | select(.name == $get_name) | .pve_pvp[].lose' ./data/user_statistics.json)
 
         clear
         echo -e "Here are the statistics for the user $get_name\n\n"
-        echo -e "Random bot:\n  wins: $win_random\n  loses: $lose_random"
+        echo -e "Easy bot:\n  wins: $win_impossible\n  loses: $lose_impossible\n"
+        echo -e "Hard bot:\n  wins: $win_impossible\n  loses: $lose_impossible\n"
+        echo -e "Random bot:\n  wins: $win_random\n  loses: $lose_random\n"
         echo -e "Impossible bot:\n  wins: $win_impossible\n  loses: $lose_impossible\n"
+        echo -e "PVP:\n  wins: $win_impossible\n  loses: $lose_impossible\n"
     else
         echo "User $get_name does not exist"
     fi
